0e1812b45fde03fc2f48236ca6edcf44e61ec502
andy.chuang <andy.chuang@lctech.com.tw>
2025-01-14 06:03:46 +0000

    fix: add argument --remove-toc

--- compile.sh
+++ compile.sh
@@ -95,7 +95,7 @@ else
     cd .. && rm -rf src && mv tmp_src src
 
     # remove TOC
-    sudo -E TAG_VERSION=$TAG_VERSION curl -sL "https://raw.githubusercontent.com/lctech-andychuang/protobuf-codegen-action/main/proto/md-formater.sh" | bash
+    # sudo -E TAG_VERSION=$TAG_VERSION curl -sL "https://raw.githubusercontent.com/lctech-andychuang/protobuf-codegen-action/main/proto/md-formater.sh" | bash
 
 fi
 
